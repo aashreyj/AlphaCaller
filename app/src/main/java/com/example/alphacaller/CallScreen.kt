81c1108601c9d190f60d89b411b508f961a7cc4a
Aashrey <aashrey9.jain@gmail.com>
2019-06-01 22:14:09 +0530

    added calling feature

--- app/src/main/java/com/example/alphacaller/CallScreen.kt
+++ app/src/main/java/com/example/alphacaller/CallScreen.kt
@@ -1,35 +1,46 @@
 package com.example.alphacaller
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.telephony.TelephonyManager
 import android.widget.TextView
+import android.widget.Toast
 import java.text.DecimalFormat
 
 class CallScreen : AppCompatActivity() {
 
+    var index: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_screen)
-
-        val numberDisplay1 = findViewById<TextView>(R.id.numbers1)
-        val numberDisplay2 = findViewById<TextView>(R.id.numbers2)
-        val numberDisplay3 = findViewById<TextView>(R.id.numbers3)
-        val numberDisplay4 = findViewById<TextView>(R.id.numbers4)
-
-        val numberList: ArrayList<Double> = (this.application as NumberLister).getList()
-
-        val df = DecimalFormat("#")
-
-        numberDisplay1.text = df.format(numberList[0])
-        numberDisplay2.text = df.format(numberList[1])
-        numberDisplay3.text = df.format(numberList[2])
-        numberDisplay4.text = df.format(numberList[3])
     }
 
     override fun onBackPressed() {
         val goToMain = Intent(this@CallScreen, MainActivity::class.java)
         startActivity(goToMain)
         this.finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val phoneNumber: TextView = findViewById(R.id.phoneNumber)
+        val numberList: ArrayList<Double> = (this.application as NumberLister).getList()
+        val df = DecimalFormat("#")
+
+        if (index < numberList.size) {
+            phoneNumber.text = df.format(numberList[index++])
+
+            Handler().postDelayed({
+                val makeCall = Intent(Intent.ACTION_CALL)
+                makeCall.data = Uri.parse("tel:" + phoneNumber.text)
+                startActivity(makeCall)
+            }, 2500)
+        }
+        else
+            Toast.makeText(this, "Finished calls...", Toast.LENGTH_SHORT).show()
     }
 }
